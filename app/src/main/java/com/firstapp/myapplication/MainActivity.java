ae6adb0e9bcfde75b1e658aab4b6282ed23b8f38
UmerTayyeb <88741948+UmerTayyeb@users.noreply.github.com>
2024-09-10 02:38:40 +0500

    Update MainActivity.java

--- app/src/main/java/com/firstapp/myapplication/MainActivity.java
+++ app/src/main/java/com/firstapp/myapplication/MainActivity.java
@@ -137,7 +137,7 @@ public class MainActivity extends AppCompatActivity {
         RequestBody requestBody = RequestBody.create(object.toString(),JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/images/generations")
-                .header("Authorization","Bearer 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")
+                .header("Authorization","API-key")
                 .post(requestBody)
                 .build();
 
